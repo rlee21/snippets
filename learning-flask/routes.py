324dd859d243c5faccf52193f2510e0d3c1302a5
relee2 <relee@avvo.com>
2017-04-20 21:27:01 -0700

    revert to tutorial version

--- learning-flask/routes.py
+++ learning-flask/routes.py
@@ -1,20 +1,18 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://localhost/robony'
+
+@app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/learningflask'
 
 @app.route("/")
 def index():
-    return render_template("main.html")
+	return render_template("index.html")
 
-
-
-
-
-
-
-
-
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
+
