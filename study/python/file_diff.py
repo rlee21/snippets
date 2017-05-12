9ce730ad4b2dbd339bb32873f075cb921702806a
rlee21 <rlee21@comcast.net>
2017-05-11 21:11:21 -0700

    refactor with list comprehensions and conditons for empty rows

--- study/python/file_diff.py
+++ study/python/file_diff.py
@@ -3,21 +3,22 @@ import csv
 
 with open("file1.csv", "r") as infile1:
     reader = csv.reader(infile1)
+#    reader = infile1.read()
+#    print(reader)
     next(reader) 
-    #file1_ids = [line[0] for line in reader]
-    file1_ids = []
-    for line in reader:
-        if line: 
-            file1_ids.append(line[0])
+    file1_ids = [line[0] for line in reader if len(line) > 0]
+#    print(file1_ids)
 
 
 with open("file2.csv", "r") as infile2:
     reader = csv.reader(infile2)
-    next(reader) 
-    results = []
-    for line in reader:
-        if line and line[0] in file1_ids:
-            results.append(line)
+    next(reader)
+    results = [line for line in reader if len(line) > 0 and line[0] not in file1_ids]
+#    results = []
+#    for line in reader:
+#        if len(line) > 0 and line[0] not in file1_ids:
+#            results.append(line)
 
+#print(results)
 for result in results:
     print(",".join(result))
