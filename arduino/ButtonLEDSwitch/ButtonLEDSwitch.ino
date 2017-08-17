301dd2a33a99af79606330b7afe1fe2155d8d27b
relee2 <relee@avvo.com>
2017-08-16 21:40:43 -0700

    add arduino

---
+++ arduino/ButtonLEDSwitch/ButtonLEDSwitch.ino
@@ -0,0 +1,43 @@
+/*
+   Button LED Switch
+*/
+
+
+int switchPin = 8;
+int ledPin = 13;
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+boolean ledOn = false;
+
+
+void setup() 
+{
+  // initialize pins as outputs
+  pinMode(switchPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+boolean debounce(boolean last)
+{
+  boolean current = digitalRead(switchPin);
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(switchPin);
+  }
+  return current;
+}
+
+
+
+void loop() 
+{
+  currentButton = debounce(lastButton);
+  if (lastButton == LOW && currentButton == HIGH)
+  {
+    ledOn = !ledOn;
+  }
+    lastButton = currentButton;
+    digitalWrite(ledPin, ledOn);
+}
+
