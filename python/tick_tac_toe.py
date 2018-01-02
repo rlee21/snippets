02f72e160c03a7dffcf426db377c0d1bbe5a8d68
relee2 <relee@avvo.com>
2018-01-01 18:05:09 -0800

    refactor tick_tac_toe.py

--- python/tick_tac_toe.py
+++ python/tick_tac_toe.py
@@ -2,60 +2,76 @@
 
 
 def create_board(positions):
-    board = dict()
+    """ Return dictionary of game board positions with empty values """
+    board = {}
     for position in positions:
         board[position] = ' '
-
     return board
 
 
 def update_board(board, position, value):
+    """ Update board with position and value """
     return board.update({position: value})
 
 
 def print_board(board):
-    print('_' + board['top-L'] + '_|_' + board['top-M'] + '_|_' + board['top-R'] + '_\n' +
-          '_' + board['mid-L'] + '_|_' + board['mid-M'] + '_|_' + board['mid-R'] + '_\n' +
-          ' ' + board['low-L'] + ' | ' + board['low-M'] + ' | ' + board['low-R'] + ' \n')
+    """ Print board """
+    print('_' + board['top-l'] + '_|_' + board['top-m'] + '_|_' + board['top-r'] + '_\n' +
+          '_' + board['mid-l'] + '_|_' + board['mid-m'] + '_|_' + board['mid-r'] + '_\n' +
+          ' ' + board['low-l'] + ' | ' + board['low-m'] + ' | ' + board['low-r'] + ' \n')
 
+
+def get_position(positions):
+    """ Prompt user for game board position """
+    while True:
+        position = input('Enter position: ').strip().lower()
+        if position in positions:
+            return position
+        print('ERROR: not a valid position, enter one of the valid positions below \n', positions)
+
+
+def get_value(values):
+    """ Prompt user for game board value """
+    while True:
+        value = input('Enter value: ').strip().lower()
+        if value in values:
+            return value
+        print('ERROR: not a valid value, enter one of the valid values below \n', values)
+
+
+def play_game(board):
+    """ Run game for total of 9 moves and check for winner after every move """
+    for _ in range(9):
+        position = get_position(positions)
+        value = get_value(values)
+        update_board(board, position, value)
+        print_board(board)
+        if board['top-l'] != ' ' and board['top-l'] == board['top-r'] and board['top-l'] == board['top-m']:
+            print('YOU WON!!')
+            print_board(board)
+            break
+        elif board['mid-l'] != ' ' and board['mid-l'] == board['mid-r'] and board['mid-l'] == board['mid-m']:
+            print('YOU WON!!')
+            print_board(board)
+            break
+        elif board['low-l'] != ' ' and board['low-l'] == board['low-r'] and board['low-l'] == board['low-m']:
+            print('YOU WON!!')
+            print_board(board)
+            break
+        elif board['top-l'] != ' ' and board['top-l'] == board['mid-m'] and board['top-l'] == board['low-r']:
+            print('YOU WON!!')
+            print_board(board)
+            break
+        elif board['top-r'] != ' ' and board['top-r'] == board['mid-m'] and board['top-r'] == board['low-l']:
+            print('YOU WON!!')
+            print_board(board)
+            break
 
 
 if __name__ == '__main__':
-    positions = ['top-L', 'top-M', 'top-R',
-                 'mid-L', 'mid-M', 'mid-R',
-                 'low-L', 'low-M', 'low-R']
-    values = ['X', 'O']
+    positions = ['top-l', 'top-m', 'top-r', 'mid-l', 'mid-m', 'mid-r', 'low-l', 'low-m', 'low-r']
+    values = ['x', 'o']
     board = create_board(positions)
-    print('valid positions:\n', positions, '\nvalid values:\n', values)
+    print('valid positions:\n', ', '.join(positions), '\nvalid values:\n', ', '.join(values), '\nctrl+c to exit:')
     print_board(board)
-    for _ in range(9):
-        position = input('Enter position: ')
-        while position not in positions:
-            print('ERROR: not a valid position, enter one of the valid positions below \n', positions)
-            position = input('Enter position: ')
-        value = input('Enter value: ').upper()
-        while value not in values:
-            print('ERROR: not a valid value, enter one of the valid values below \n', values)
-            value = input('Enter value: ').upper()
-        update_board(board, position, value)
-        if board['top-L'] != ' ' and board['top-L'] == board['top-R'] and board['top-L'] == board['top-M']:
-            print('YOU WON!!')
-            print_board(board)
-            break
-        elif board['mid-L'] != ' ' and board['mid-L'] == board['mid-R'] and board['mid-L'] == board['mid-M']:
-            print('YOU WON!!')
-            print_board(board)
-            break
-        elif board['low-L'] != ' ' and board['low-L'] == board['low-R'] and board['low-L'] == board['low-M']:
-            print('YOU WON!!')
-            print_board(board)
-            break
-        elif board['top-L'] != ' ' and board['top-L'] == board['mid-M'] and board['top-L'] == board['low-R']:
-            print('YOU WON!!')
-            print_board(board)
-            break
-        elif board['top-R'] != ' ' and board['top-R'] == board['mid-M'] and board['top-R'] == board['low-L']:
-            print('YOU WON!!')
-            print_board(board)
-            break
-        print_board(board)
+    play_game(board)
