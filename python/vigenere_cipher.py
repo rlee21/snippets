03f3f02c9d379a23a66fb3dfcccbb6c2349b5795
relee2 <relee@avvo.com>
2018-02-27 21:56:50 -0800

    refactor vigenere_cipher.py

--- python/vigenere_cipher.py
+++ python/vigenere_cipher.py
@@ -1,34 +1,52 @@
+## sudo code
+# ex: key = ABC plaintext = HELLO
+# formula: c = (p + k) % 26
+# convert char in key to idx wrapping around
+# if space in plaintext don't advance idx of key
+# iter thru plaintext and key converting each char in plaintext and shift by idx of key
+# advance to next letter in keyword only if char in plaintext is a letter
+# perserve case
 
 
-def get_ciphertext(key, plaintext):
+def get_ciphertext(key, pt):
     """ Convert plaintext into ciphertext based on key using Vigenere’s cipher """
-    #formula = (plaintext + key) % 26
-    # TODO: make case insensitive
-    # TODO: fix to handle spaces e.g. key='bacon' and plaintext='Meet me at the park at eleven am'
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    alpha2nums = {}
-    data = list(zip(letters, [i for i in range(26)]))
-    for item in data:
-        alpha2nums[item[0]] = item[1]
-    nums2alpha = {}
-    for k, v in alpha2nums.items():
-        nums2alpha[v] = k
-    p_len = len(plaintext)
-    k_len = len(key)
-    key_nums = [i % k_len for i in range(p_len)]
+    # TODO: fix when plaintext is 'I like you' and key is 'pandas' because of wrapping issue
     ciphertext = ''
-    for idx, i in enumerate(plaintext):
-        ciphernum = alpha2nums[i] + key_nums[idx]
-        char = nums2alpha[ciphernum]
-        ciphertext += char 
-    
+    pt_len = len(pt)
+    key_len = len(key)
+    pt_pos = 0
+    key_pos = 0
+    for i in range(pt_len):
+        if pt[pt_pos].isalpha() and pt[pt_pos].isupper():
+            key_idx = key_pos % key_len
+            idx_ptchar = ord(pt[pt_pos]) - 65
+            idx_keychar = ord(key[key_idx]) - 65
+            encrypt_char = chr(((idx_ptchar + idx_keychar) % 26)+ 65)
+            ciphertext += encrypt_char
+            pt_pos += 1
+            key_pos += 1
+        elif pt[pt_pos].isalpha() and pt[pt_pos].islower():
+            key_idx = key_pos % key_len
+            idx_ptchar = ord(pt[pt_pos]) - 97
+            idx_keychar = ord(key[key_idx]) - 97
+            encrypt_char = chr(((idx_ptchar + idx_keychar) % 26) + 97)
+            ciphertext += encrypt_char
+            pt_pos += 1
+            key_pos += 1
+        else:
+            ciphertext += pt[pt_pos]
+            pt_pos += 1
+
     return ciphertext
 
 
 if __name__ == '__main__':
-    key = input('Enter key: ')
-    for char in key:
-        if not char.isalpha():
-            raise TypeError('key must consist of alphabetic characters only')
-    plaintext = input('Enter plaintext: ')
-    print(get_ciphertext(key, plaintext))
+    while True:
+        key = input('Enter alphabetical key: ')
+        if key.isalpha():
+            pt = input('Enter plaintext: ')
+            print(get_ciphertext(key, pt))
+            break
+        else:
+            print('ERROR: key must consist of alphabetical characters')
+
