efa30830cfba4f0423b85519da6727783f10affc
rlee21 <rlee21@comcast.net>
2018-11-21 12:44:33 -0800

    initial commit

---
+++ ruby/hello.rb
@@ -0,0 +1,29 @@
+#puts "Hello world!"
+numbers = [1, 2, 3, 4, 5]
+
+#for number in numbers
+#puts number
+#end
+
+numbers.each do |number|
+puts number
+end
+
+# foo = []
+# i = 0
+# while i < 6
+# puts "appending #{i}"
+# foo.push(i)
+# i += 1
+# puts "foo has been update: ", foo
+# end
+
+# 11.times do |i|
+#   puts i
+# end
+
+# foo = (0..5).to_a
+# puts foo
+
+# foo = Range.new(0, 5)
+#   puts foo
